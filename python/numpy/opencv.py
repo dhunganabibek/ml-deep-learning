0966ce6271ed62716466689efe7c681ec5a67bc6
Bibek Dhungana <dhunganabibek2016@gmail.com>
2025-07-10 23:42:47 -0500

    Applied boolean masking to image:

--- python/numpy/opencv.py
+++ python/numpy/opencv.py
@@ -4,5 +4,5 @@ import numpy as np
 img = np.array([np.arange(0,10),np.arange(10,20), np.arange(30, 40)])
 img = np.random.rand(100,200)* 100
 img = img.astype(np.uint8)
-cv2.imshow('Rando',img)
+cv2.imshow('Random',img)
 cv2.waitKey(0)
